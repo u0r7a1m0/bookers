e28f53b119884a1bf88ee8e3bf8d72850130e883
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2022-11-13 10:08:38 +0000

    [Update]3-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,15 @@
 class BooksController < ApplicationController
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice]="Book was successfully created."
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      # 投稿成功した場合
+      flash[:success]="Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      # 投稿が失敗した場合
+      @books=Book.all
+      render :index
+    end
   end
   def index
     @books=Book.all
@@ -18,14 +24,20 @@ class BooksController < ApplicationController
     @book=Book.find(params[:id])
   end
   def update
-    book=Book.find(params[:id])
-    book.update(list_params)
-    redirect_to book_path(book.id)
+    @book=Book.find(params[:id])
+    
+    if @book.update(book_params)
+      flash[:update]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
-    redirect_to books_path  # 投稿一覧画面へリダイレクト  
+    redirect_to books_path  # 投稿一覧画面へリダイレクト
+    
   end
 
   private
